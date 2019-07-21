7096ab00c63c213a0f43d08857fa9334e00c45ef
AlfaRiza <malfariza45@gmail.com>
2019-07-21 16:29:33 +0700

    menambahkan fitur pencarian data memanfaatkan session

--- application/views/peoples/index.php
+++ application/views/peoples/index.php
@@ -1,8 +1,20 @@
 <div class="container">
+    <h3 class="mt-3">List of Peoples</h3>
+    <div class="row">
+        <div class="col-md-4">
+            <form method="post" action="<?= base_url('peoples'); ?>">
+                <div class="input-group mb-3">
+                    <input type="text" class="form-control" placeholder="Search keyword" name="keyword" autocomplete="off" autofocus>
+                    <div class="input-group-append">
+                        <input class="btn btn-primary" type="submit" name="submit" id="button-addon2"></input>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
     <div class="row mt-3">
         <div class="col-md-10">
-            <h3>List of Peoples</h3>
-
+            <h5>Result : <?= $total_rows; ?> </h5>
             <table class="table">
                 <thead>
                     <tr>
@@ -13,6 +25,16 @@
                     </tr>
                 </thead>
                 <tbody>
+                    <?php
+                    if (empty($peoples)) : ?>
+                        <tr>
+                            <td colspan="4">
+                                <div class="alert alert-danger" role="alert">
+                                    Data not found
+                                </div>
+                            </td>
+                        </tr>
+                    <?php endif; ?>
                     <?php
                     foreach ($peoples as $people) :  ?>
                         <tr>
